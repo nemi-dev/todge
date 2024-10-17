d272832e01d780e5b802437486e78ba210870510
pvpanime <pvpanime@nemi.dev>
2024-10-17 13:44:28 +0900

    todge change

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,23 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   // root: 'www',
+  build: {
+    rollupOptions: {
+      input: {
+        todge: "todge.html"
+      },
+      output: {
+        // Customize the output filenames
+        entryFileNames: '[name].[hash].js',     // Entry points (JavaScript)
+        chunkFileNames: '[name].[hash].js',     // Dynamic imports (JavaScript)
+        assetFileNames: '[name].[hash][extname]',
+        // assetFileNames: ({ name }) => {
+        //   if (name.endsWith('.css')) {
+        //     return 'assets/css/[name].[hash][extname]';   // CSS files
+        //   }
+        //   return 'assets/[name].[hash][extname]';         // Other assets like images, fonts
+        // },
+      },
+    },
+  },
 });
